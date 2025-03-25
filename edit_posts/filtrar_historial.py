63be59124a0b51e0f2c602f911863cf22621b944
miguelrivera.g64@gmail.com <miguelrivera.g64@gmail.com>
2025-03-24 23:47:43 -0600

    Better filter logic, but only works if date interval is within the same day...so far!

--- edit_posts/filtrar_historial.py
+++ edit_posts/filtrar_historial.py
@@ -6,16 +6,17 @@ pd.options.mode.chained_assignment = None  # default='warn'
 '''
 Filtra el historial exportado de Microsoft edge para contener solo los resultados
 en cierto intervalo de fechas y obtener los IDs de posts de Danbooru que se 
-quieren modificar. Prueba en nueva rama
+quieren modificar.
 '''
 
-def create_date_string(year:int, month:int, day:int, hours:int, minutes: int, seconds:int = 0)-> tuple:
+def create_date_string(year:int, month:int, day:int, hours:int, minutes: int, seconds:int = 0)-> tuple[str]:
+    # Returns as string, not datetime object
+    date = dt.date(year, month, day).strftime(r'%Y-%m-%d')
+    time = dt.time(hours, minutes, seconds).strftime(r'%H:%M:%S')
 
-    date = (dt.date(year, month, day), dt.time(hours, minutes, seconds))
+    return (date, time)
 
-    return date
-
-def filter_by_date(df: pd.DataFrame, after_date:str, before_date:str) -> pd.DataFrame:
+def filter_by_date_old(df: pd.DataFrame, after_date:str, before_date:str) -> pd.DataFrame:
 
     # Filters dataframe of MicroEdge browsing history export by date interval, doesn't take account for microseconds
 
@@ -36,17 +37,22 @@ def filter_by_date(df: pd.DataFrame, after_date:str, before_date:str) -> pd.Data
 
     return filtered_df
 
-def filter_by_date3(df: pd.DataFrame, after_date:str, before_date:str) -> pd.DataFrame:
+def filter_by_date(df: pd.DataFrame, after_date, before_date) -> pd.DataFrame:
 
-    df['DateTime'] = pd.to_datetime(df['DateTime'].str[:19])
-    df.set_index('DateTime') # Sets datetime as index, allows very fasts filters
+    #df['DateTime'] = pd.to_datetime(df['DateTime'].str[:19])
+    df['DateTime'] = pd.to_datetime(df['DateTime']).dt.tz_localize(None)
+    df.set_index('DateTime', inplace= True) # Sets datetime as index (pandas.DateTimeIndex), allows very fasts filters
+    
+    if after_date[0] == before_date[0]:
+        # Este es el caso de que el filtro sea de dos fechas en el mismo dia 
+        filtered_df = df.loc[after_date[0]].between_time(after_date[1], before_date[1])
+    else:
+        print(df.index < '2025-03-24') # Se permite esta clase de comparaciones con el indice
+        raise Exception('TODAVIA NO IMPLEMENTO ESTA PVTA MIERDA')
+         #filtered_df = df.loc[filter1 & filter2] ###### get_loc()??
 
-    print(df, "aver")
-    '''  
-    filtered_df.set_index('DateTime')
-    '''
-
-    filtered_df = df
+    print(df.shape[0] - filtered_df.shape[0], ' rows removed by date filter')
+    if filtered_df.empty: print('Date filter returned an empty dataframe')
 
     return filtered_df
 
@@ -54,20 +60,26 @@ def filter_by_danbooru_posts(df: pd.DataFrame, test: bool) -> pd.DataFrame:
 
     # Filters dataframe of MicroEdge browsing history export by url that belongs to Danbooru or Testbooru posts
 
+    df.rename(columns={'NavigatedToUrl': 'PostID'}, inplace= True)
     if df.empty: return df
 
     if test:
-        filtered_df = df[df['NavigatedToUrl'].str[:34] == 'https://testbooru.donmai.us/posts/']
-        filtered_df['NavigatedToUrl'] = filtered_df['NavigatedToUrl'].str[34:]
+        filtered_df = df[df['PostID'].str[:34] == 'https://testbooru.donmai.us/posts/']
+        filtered_df['PostID'] = filtered_df['PostID'].str[34:]
 
     else:
-        filtered_df = df[df['NavigatedToUrl'].str[:33] == 'https://danbooru.donmai.us/posts/']
-        filtered_df['NavigatedToUrl'] = filtered_df['NavigatedToUrl'].str[33:]
+        filtered_df = df[df['PostID'].str[:33] == 'https://danbooru.donmai.us/posts/']
+        filtered_df['PostID'] = filtered_df['PostID'].str[33:]
 
     # Get just post ID from URL
-    filtered_df['NavigatedToUrl'] = filtered_df['NavigatedToUrl'].str.split('?', expand=True)[0]
-    filtered_df.rename(columns={'NavigatedToUrl': 'PostID'}, inplace= True)
 
+    if filtered_df.empty: 
+        print('No posts found in the interval given')
+        return filtered_df
+
+    filtered_df['PostID'] = filtered_df['PostID'].str.split('?', expand=True)[0]
+
+    print(df.shape[0] - filtered_df.shape[0], ' rows removed by posts filter')
     if filtered_df.empty: print('No posts found in the interval given')
 
     return filtered_df
@@ -80,14 +92,17 @@ def main(FILE: str, after_date, before_date, TEST: bool = True) -> list:
         raise Exception('- Error: before_date time must be earlier than after_date')
 
     df: pd.DataFrame = pd.read_csv(FILE, usecols=['DateTime', 'NavigatedToUrl'])
-    print(after_date, before_date)
+    print('Filtering {0} registers'.format(df.shape[0]), after_date, before_date)
+
     df = filter_by_date(df, after_date, before_date)
 
     df = filter_by_danbooru_posts(df, test= TEST)
     
     print(df, '\n', df.shape[0], ' datos')
 
-    return list(df['PostID'])
+    output =  list(df['PostID'])
+
+    return set(output)
 
 if __name__ == '__main__':
 
