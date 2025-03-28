17dacb3e9a669a4bea51cb9636a75dc7c5d309e1
miguelrivera.g64@gmail.com <miguelrivera.g64@gmail.com>
2025-03-27 21:29:46 -0600

    El indice para filtrar funciona, pero aun falta ajustar la funcion filter_by_date

--- edit_posts/filtrar_historial.py
+++ edit_posts/filtrar_historial.py
@@ -9,51 +9,34 @@ en cierto intervalo de fechas y obtener los IDs de posts de Danbooru que se
 quieren modificar.
 '''
 
-def create_date_string(year:int, month:int, day:int, hours:int, minutes: int, seconds:int = 0)-> tuple[str]:
+def create_date_string(DATE: list[int], TIME: list[int])-> tuple[str]:
     # Returns as string, not datetime object
-    date = dt.date(year, month, day).strftime(r'%Y-%m-%d')
-    time = dt.time(hours, minutes, seconds).strftime(r'%H:%M:%S')
+    date = dt.date(DATE[0], DATE[1], DATE[2]).strftime(r'%Y-%m-%d')
+    time = dt.time(TIME[0], TIME[1], TIME[2]).strftime(r'%H:%M:%S')
+    date_time = dt.datetime(DATE[0], DATE[1], DATE[2], TIME[0], TIME[1], TIME[2]).strftime(r'%Y-%m-%d %H:%M:%S')
 
-    return (date, time)
-
-def filter_by_date_old(df: pd.DataFrame, after_date:str, before_date:str) -> pd.DataFrame:
-
-    # Filters dataframe of MicroEdge browsing history export by date interval, doesn't take account for microseconds
-
-    df['DateTime'] = pd.to_datetime(df['DateTime'].str[:19]) # yyyy-mm-ddThh:mm:ss
-
-    filter_mask_after_date =  ((df['DateTime'].dt.date == after_date[0])  & (df['DateTime'].dt.time >= after_date[1]))
-
-    if before_date != None:
-        filter_mask_between_dates = ((df['DateTime'].dt.date > after_date[0]) & (df['DateTime'].dt.date < before_date[0]))
-        filter_mask_before_date = ((df['DateTime'].dt.date == before_date[0]) & (df['DateTime'].dt.time <= before_date[1]))
-        filter_mask = filter_mask_between_dates | (filter_mask_after_date & filter_mask_before_date) 
-    else:
-        filter_mask = filter_mask_after_date
-
-    filtered_df = df[filter_mask]
-
-    if filtered_df.empty: print('Date filter returned an empty dataframe')
-
-    return filtered_df
+    return (date, time, date_time)
 
 def filter_by_date(df: pd.DataFrame, after_date, before_date) -> pd.DataFrame:
 
-    #df['DateTime'] = pd.to_datetime(df['DateTime'].str[:19])
-    df['DateTime'] = pd.to_datetime(df['DateTime']).dt.tz_localize(None)
-    df.set_index('DateTime', inplace= True) # Sets datetime as index (pandas.DateTimeIndex), allows very fasts filters
-    
-    if after_date[0] == before_date[0]:
-        # Este es el caso de que el filtro sea de dos fechas en el mismo dia 
+    # metodo alternativo: usar df['DateTime'].dt.date / dt.time
+    #
+
+    df['DateTime'] = pd.to_datetime(df['DateTime']).dt.tz_localize(None) #.str[:19] to get datetime without miliseconds
+    df.set_index('DateTime', inplace= True) # Sets datetime as pandas.DateTimeIndex
+
+    if after_date[0] == before_date[0]: # same day case
         filtered_df = df.loc[after_date[0]].between_time(after_date[1], before_date[1])
     else:
-        print(df.index < '2025-03-24') # Se permite esta clase de comparaciones con el indice
-        raise Exception('TODAVIA NO IMPLEMENTO ESTA PVTA MIERDA')
+        df.index.get_loc('yyyy-mm-dd hh:mm:ss') # tambien se puede recortar a hh:mm
+
          #filtered_df = df.loc[filter1 & filter2] ###### get_loc()??
 
     print(df.shape[0] - filtered_df.shape[0], ' rows removed by date filter')
     if filtered_df.empty: print('Date filter returned an empty dataframe')
-
+    '''
+    NOTAS: CAMBIAR CREATE DATE PARA DEVOLVER OBJETO DE DATETIME Y SOLO CONVERTIR A TEXTO DENTRO DE ESTA FUNCION?
+    '''
     return filtered_df
 
 def filter_by_danbooru_posts(df: pd.DataFrame, test: bool) -> pd.DataFrame:
@@ -84,18 +67,25 @@ def filter_by_danbooru_posts(df: pd.DataFrame, test: bool) -> pd.DataFrame:
 
     return filtered_df
 
-def main(FILE: str, after_date, before_date, TEST: bool = True) -> list:
+def date_verifier(after_date, before_date):
 
     if (after_date[0] > before_date[0]):
         raise Exception('- Error: before_date must be a date before after_date')
+    
     elif (after_date[0] == before_date[0]) and (after_date[1] > before_date[1]):
         raise Exception('- Error: before_date time must be earlier than after_date')
+    
+    else: return
+
+def main(FILE: str, after_date, before_date, TEST: bool = True) -> set:
+
+    date_verifier(after_date, before_date)
 
     df: pd.DataFrame = pd.read_csv(FILE, usecols=['DateTime', 'NavigatedToUrl'])
     print('Filtering {0} registers'.format(df.shape[0]), after_date, before_date)
 
     df = filter_by_date(df, after_date, before_date)
-
+    pass
     df = filter_by_danbooru_posts(df, test= TEST)
     
     print(df, '\n', df.shape[0], ' datos')
@@ -107,7 +97,9 @@ def main(FILE: str, after_date, before_date, TEST: bool = True) -> list:
 if __name__ == '__main__':
 
     archivo = r'C:\Users\Usuario\Desktop\Programacion\Python\Online\danbooru_resources\historial\BrowserHistory.csv'
-    AFTER_DATE = create_date_string(2025, 3, 24, hours=20, minutes=16, seconds=0)
-    BEFORE_DATE = create_date_string(2025, 3, 24, hours=20, minutes=18, seconds=0)
 
-    main(archivo, AFTER_DATE, BEFORE_DATE, TEST= False)
+    dia = (2025, 3, 25)
+    AFTER_DATE = create_date_string(dia, (16, 34, 0))
+    BEFORE_DATE = create_date_string(dia, (16, 40, 0))
+
+    print(main(archivo, AFTER_DATE, BEFORE_DATE, TEST= False))
--- edit_posts/update_post_tags.py
+++ edit_posts/update_post_tags.py
@@ -13,15 +13,22 @@ def create_auth() -> tuple:
 def check_rating() -> bool:
     pass
 
+def check_tags() -> str:
+    pass
+
 def main(rating: str = None, tags: str = None, post_ids: list = None, login_args: tuple[str] = None) -> None:
 
     client = requests.Session() # Inicia una sesion persistente
     
     check_rating()
 
+    tags += check_tags()
+
+    if rating != None: rating = str.lower(rating)
+
     payload =  {
-        'post[rating]': rating, 
-        'post[tag_string]': tags
+                'post[rating]': rating, 
+                'post[tag_string]': tags
                }
 
     for post_id in post_ids:
@@ -38,5 +45,13 @@ def main(rating: str = None, tags: str = None, post_ids: list = None, login_args
 if __name__ == '__main__':
 
     LOGIN_ARGS = create_auth()
-    ids = [29954, 29937, 29924]
-    main('G', posts_ids = ids, login_args = LOGIN_ARGS)
+
+    import filtrar_historial as fhl
+    archivo = r'C:\Users\Usuario\Desktop\Programacion\Python\Online\danbooru_resources\historial\BrowserHistory.csv'
+    dia = (2025, 3, 27)
+    AFTER_DATE = fhl.create_date_string(dia, (22, 58, 30))
+    BEFORE_DATE = fhl.create_date_string(dia, (23, 59, 59))
+    ids = fhl.main(archivo, AFTER_DATE, BEFORE_DATE, TEST= False)
+    ids = sorted(list(ids), reverse= True)
+    print(ids)
+    main(tags='shielding_another_from_rain', post_ids = ids, login_args = LOGIN_ARGS)
